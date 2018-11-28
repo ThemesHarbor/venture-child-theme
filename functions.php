2b1f4c6448f1b1ce976e6f393bcabd813a746e6d
Taras Dashkevych <trdashix@gmail.com>
2018-11-27 16:58:17 -0800

    Remove: enqueue parent style function (now this style is loaded via the parent theme);

--- functions.php
+++ functions.php
@@ -9,22 +9,12 @@
  * @since 1.0.0
  */
 
- /**
-  *  Sets up child theme defaults and registers support for various WordPress features.
-  *
-  * @link http://codex.wordpress.org/Child_Themes
-  */
- function venture_child_theme_setup() {
-     load_child_theme_textdomain( 'venture', get_stylesheet_directory() . '/languages' );
- }
- add_action( 'after_setup_theme', 'venture_child_theme_setup' );
-
 /**
- * Enqueue parent style.
+ *  Sets up child theme defaults and registers support for various WordPress features.
  *
  * @link http://codex.wordpress.org/Child_Themes
  */
-function venture_child_enqueue_parent_styles() {
-    wp_enqueue_style( 'venture-parent-style', get_template_directory_uri() . '/style.css', array( 'venture-css-framework' ) );
+function venture_child_theme_setup() {
+     load_child_theme_textdomain( 'venture', get_stylesheet_directory() . '/languages' );
 }
-add_action( 'wp_enqueue_scripts', 'venture_child_enqueue_parent_styles' );
+add_action( 'after_setup_theme', 'venture_child_theme_setup' );
